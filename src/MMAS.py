3c7eea8c364b0dbfce718fb72d73c633f6d10947
Kai-Ref <555kaireffert@gmail.com>
2025-10-05 13:19:39 +0200

    run results on all data sets

--- src/MMAS.py
+++ src/MMAS.py
@@ -1,27 +1,24 @@
 from ioh import get_problem, ProblemClass
 from ioh import logger
-import sys
-import numpy as np
-from tqdm import tqdm
-from aco import ACO
 import argparse
 import time
+from tqdm import tqdm
+from aco import ACO
 
 
-# Please replace this `random search` by your `genetic algorithm`.
-def mmas(func, budget=None, evaporation_rate=0.02):
-    # budget of each run: 50n^2
+# MMAS implementation
+def mmas(func, budget=None, evaporation_rate=0.02, strict=False):
     if budget is None:
         budget = int(func.meta_data.n_variables * func.meta_data.n_variables * 50)
+
     if func.meta_data.problem_id == 18 and func.meta_data.n_variables == 32:
         optimum = 8
     else:
         optimum = func.optimum.y
-    # print(f"The optimal value is {optimum}")
-    # 10 independent runs for each algorithm on each problem.
+
     start = time.time()
     average_time_70 = 0
-    for _ in tqdm(range(100),desc="Runs"):
+    for _ in tqdm(range(10), desc=f"Runs on F{func.meta_data.problem_id}"):
         aco = ACO(number_bits=func.meta_data.n_variables, evaporation_rate=evaporation_rate)
         f_opt = 0.0
         time_70 = None
@@ -30,7 +27,9 @@ def mmas(func, budget=None, evaporation_rate=0.02):
         for _ in range(budget):
             x = aco()
             f = func(x)
-            if f >= f_opt or x_opt is None:
+
+            # --- difference between MMAS and MMAS* ---
+            if (not strict and (f >= f_opt or x_opt is None)) or (strict and (f > f_opt or x_opt is None)):
                 f_opt = f
                 x_opt = x
             if f_opt >= optimum:
@@ -39,39 +38,60 @@ def mmas(func, budget=None, evaporation_rate=0.02):
                 time_70 = time.time() - start_this_run
                 average_time_70 += time_70
             aco.update_pheromone(x_opt)
-        # print(f"Best found solution: f={f_opt}, x={x_opt}")
         func.reset()
     end = time.time()
-    print(f"Total time for 100 runs: {end - start} seconds, average time per run: {(end - start) / 100} seconds, evaporation rate: {evaporation_rate}, average time to reach 70% of optimum: {average_time_70 / 100} seconds")
+    print(f"[F{func.meta_data.problem_id}] Total time for 100 runs: {end - start:.2f}s,"
+          f" avg per run: {(end - start) / 100:.2f}s,"
+          f" evaporation: {evaporation_rate},"
+          f" avg time to reach 70%: {average_time_70 / 100:.2f}s")
     return f_opt, x_opt
 
 
-# Declaration of problems to be tested.
-n=100
-max_iter=100000
-om = get_problem(fid=1, dimension=n, instance=1, problem_class=ProblemClass.PBO)
-lo = get_problem(fid=2, dimension=n, instance=1, problem_class=ProblemClass.PBO)
-labs = get_problem(fid=18, dimension=n, instance=1, problem_class=ProblemClass.PBO)
-# Create default logger compatible with IOHanalyzer
-# `root` indicates where the output files are stored.
-# `folder_name` is the name of the folder containing all output. You should
-# compress this folder and upload it to IOHanalyzer
+# Parameters
+n = 100
+max_iter = 100000
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--evaporation_rate', type=float, default=1, help='Evaporation rate for MMAS')
 args = parser.parse_args()
 evaporation_rate = args.evaporation_rate
 
+# Logger
 l = logger.Analyzer(
     root="data",
     folder_name=f"mmas_{evaporation_rate}_evap",
     algorithm_name=f"mmas_{evaporation_rate}",
     algorithm_info=f"test of IOHexperimenter in python, evaporation_rate={evaporation_rate}",
 )
-om.attach_logger(l)
-mmas(om,budget=max_iter, evaporation_rate=evaporation_rate)
-lo.attach_logger(l)
-mmas(lo,budget=max_iter, evaporation_rate=evaporation_rate)
-labs.attach_logger(l)
-mmas(labs,budget=max_iter, evaporation_rate=evaporation_rate)
-# This statemenet is necessary in case data is not flushed yet.
+
+# List of problem IDs
+problem_ids = [1, 2, 3, 18, 23, 24, 25]
+
+# Run MMAS on each problem
+for fid in problem_ids:
+    func = get_problem(fid=fid, dimension=n, instance=1, problem_class=ProblemClass.PBO)
+    func.attach_logger(l)
+    mmas(func, budget=max_iter, evaporation_rate=evaporation_rate, strict=False)
+
+# Flush logger
+del l
+
+# Logger
+l = logger.Analyzer(
+    root="data",
+    folder_name=f"mmas_star_{evaporation_rate}_evap",
+    algorithm_name=f"mmas_star_{evaporation_rate}",
+    algorithm_info=f"test of IOHexperimenter in python, evaporation_rate={evaporation_rate}",
+)
+
+# List of problem IDs
+problem_ids = [1, 2, 3, 18, 23, 24, 25]
+
+# Run MMAS on each problem
+for fid in problem_ids:
+    func = get_problem(fid=fid, dimension=n, instance=1, problem_class=ProblemClass.PBO)
+    func.attach_logger(l)
+    mmas(func, budget=max_iter, evaporation_rate=evaporation_rate, strict=True)
+
+# Flush logger
 del l
